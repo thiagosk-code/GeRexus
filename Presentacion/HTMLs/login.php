bd3aaaab8a671f491b88ff4f64e1e2313b4f24ac
juano1503 <juanoliceo@gmail.com>
2026-09-04 16:56:03 -0300

    login completo espero

--- Presentacion/HTMLs/login.php
+++ Presentacion/HTMLs/login.php
@@ -1,14 +1,88 @@
 <?php
-require_once __DIR__ . '/../Scripts/lang.php';
-?>
 
+require_once (__DIR__ . '/../../Logica/FachadaLogica.php');
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+$mensajeError = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $tokenEnviado = $_POST['csrf_token'] ?? '';
+
+    if (!hash_equals($_SESSION['csrf_token'], $tokenEnviado)) {
+        $mensajeError = "Peticion no valida (Error CSRF).";
+    }else {
+        $email = $_POST['email'] ?? '';
+        $contra = $_POST['password'] ?? '';
+        $captchaToken = $_POST['g-recaptcha-response'] ?? '';
+
+        if (!($_POST['email'] === '' || $_POST['password'] === '')){
+
+            $facha = new FachadaLogica();
+            $res = $facha->retornoIlogicaUsuario()->IniciarSesionL($email, $contra, $captchaToken);
+
+            if ($res['exito'] === true) {
+                session_regenerate_id(true);
+
+                $_SESSION['idUsuario'] = $res['idUsuario'];
+                $_SESSION['esAdmin'] = false;
+                $_SESSION['login_time'] = time();
+
+                header("Location: base.php");
+                exit();
+            }else{
+                $mensajeError = $res['mensaje'];
+            }
+            
+        }else{
+            $mensajeError = "Hay campos sin completar";
+        }
+    }
+    
+}
+
+if (isset($_GET['lang'])) {
+    if ($_GET['lang'] === 'en') {
+        $_SESSION['lang'] = 'en';
+    } else {
+        $_SESSION['lang'] = 'es';
+    }
+}
+
+$lang = (isset($_SESSION['lang']) && $_SESSION['lang'] === 'en') ? 'en' : 'es';
+
+$dic = [
+    'es' => [
+        'registrarse' => 'Registrarse',
+        'iniciar_sesion' => 'Iniciar Sesion',
+        'correo' => 'Correo',
+        'contrasena' => 'Contraseña',
+        'no_cuenta' => '¿No tienes una cuenta?'
+    ],
+    'en' => [
+        'registrarse' => 'Sign up',
+        'iniciar_sesion' => 'Log in',
+        'correo' => 'Email',
+        'contrasena' => 'Password',
+        'no_cuenta' => "Don't have an account?"
+    ]
+];
+
+$txt = $dic[$lang];
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $lang; ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Draftoicos - <?php echo $txt['iniciar_sesion']; ?></title>
-    <link rel="icon" type="image/png" href="../../Assets/Socrates.png">
+    <link rel="icon" type="image/png" href="../Assets/SocratesPNG.png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,6 +91,7 @@ require_once __DIR__ . '/../Scripts/lang.php';
 
     <link rel="stylesheet" href="../CSSs/style.css">
     <link rel="stylesheet" href="../CSSs/login.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
 
@@ -44,7 +119,8 @@ require_once __DIR__ . '/../Scripts/lang.php';
     <main class="login-container">
         <h1 class="login-title"><?php echo $txt['iniciar_sesion']; ?></h1>
         
-        <form class="login-form" action="/tu-ruta-login" method="POST">
+        <form class="login-form" action="login.php" method="POST">
+            <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
             <div class="inputs-container">
                 <div class="input-group">
                     <span class="material-symbols-outlined field-icon">mail</span>
@@ -56,10 +132,16 @@ require_once __DIR__ . '/../Scripts/lang.php';
                     <input type="password" name="password" placeholder="<?php echo $txt['contrasena']; ?>" required autocomplete="current-password">
                 </div>
             </div>
-
+            <div class="recaptcha-wrapper">
+                <div class="g-recaptcha" data-sitekey="6Lc2npAtAAAA
+                AHQhPwUsh3USUPIpKxiftXoNdcAg"></div>
+            </div>
             <div class="bottom-container">
-                <button type="submit" class="btn-submit"><?php echo $txt['iniciar_sesion']; ?></button>
+                <?php if ($mensajeError !== ''): ?>
+                <p class="msg-status error"><?php echo $mensajeError; ?></p>
+                <?php endif; ?>
                 <a href="register.php" class="register-link"><?php echo $txt['no_cuenta']; ?></a>
+                <input type="submit" name="btn_login" value="<?php echo $txt['iniciar_sesion']; ?>" class="btn-submit">
             </div>
         </form>
     </main>
